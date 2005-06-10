b571177481093ec58f80168cfac4ff92d9d2e311
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2005-06-10 01:40:17 +0000

    Timeout based call back added
    git-svn-id: https://svn.code.sf.net/p/freemat/code/branches/QTDEVEL@1124 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/src/mac/main.cpp
+++ FreeMat2/src/mac/main.cpp
@@ -6,51 +6,17 @@
 #include "MainApp.hpp"
 #include <qapplication.h>
 
-using namespace FreeMat;
-
-#include "BaseTerminal.hpp"
-#include "Module.hpp"
-#include "Class.hpp"
-#include "LoadCore.hpp"
-#include "LoadFN.hpp"
-#include "GraphicsCore.hpp"
-#include "File.hpp"
-
-
-int RunMainApp(BaseTerminal* term, std::string helpPath) {
-  Context *context = new Context;
-  
-  LoadModuleFunctions(context);
-  LoadClassFunction(context);
-  LoadCoreFunctions(context);
-  LoadFNFunctions(context);
-  LoadGraphicsCoreFunctions(context);  
-  InitializeFigureSubsystem();
-
-  const char *envPtr;
-  envPtr = getenv("FREEMAT_PATH");
-  term->setContext(context);
-  if (envPtr)
-    term->setPath(std::string(envPtr));
-  else 
-    term->setPath(std::string(""));
-  WalkTree *twalk = new WalkTree(context,term);
-  term->outputMessage(" Freemat v2.0");
-  term->outputMessage("\n");
-  term->outputMessage(" Copyright (c) 2002-2005 by Samit Basu\n");
-  while (twalk->getState() != FM_STATE_QUIT) {
-    twalk->resetState();
-    twalk->evalCLI();
-  }
-  return 0;
-}
-
 int main(int argc, char *argv[]) {
   QApplication app(argc, argv, TRUE);
+  MainApp m_app;
+  QTimer *m_start;
   
   GUITerminal *iterm = new GUITerminal;
   iterm->show();
   iterm->resizeTextSurface();
-  
-  return RunMainApp(iterm, "");
+  m_app.SetTerminal(iterm);
+  m_start = new QTimer;
+  QObject::connect(m_start,SIGNAL(timeout()),&m_app,SLOT(Run()));
+  m_start->start(10,TRUE);
+  return app.exec();
 }
