a7c69cfe88912ec7a0bac0492dd9db88df5d58c5
geneing <geneing@58e251e6-400e-0410-b350-93cf1ec51ee9>
2007-09-18 06:55:44 +0000

    Adding lapack build to dependencies.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/branches/cmake_build@2890 58e251e6-400e-0410-b350-93cf1ec51ee9

---
+++ dependencies/lapack/install/second.f
@@ -0,0 +1,25 @@
+      REAL             FUNCTION SECOND( )
+*
+*  -- LAPACK auxiliary routine (version 2.0) --
+*     Univ. of Tennessee, Univ. of California Berkeley, NAG Ltd.,
+*     Courant Institute, Argonne National Lab, and Rice University
+*     September 30, 1994
+*
+*  Purpose
+*  =======
+*
+*  SECOND returns the user time for a process in seconds.
+*  This version gets the time from the system timer clock().
+*
+* =====================================================================
+*
+      USE DFPORT
+*     ..
+*     .. Executable Statements ..
+*
+      SECOND = SECNDS( 0.0E0 )
+      RETURN
+*
+*     End of SECOND
+*
+      END
