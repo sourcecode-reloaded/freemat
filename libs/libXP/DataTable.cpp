b8e58bfa7279d627438625f60df87e507fe9ef38
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2008-12-26 07:07:18 +0000

    A new widget -- a multi-column list widget -- will use it for the file and variable browsers.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/branches/FreeMat4@3503 58e251e6-400e-0410-b350-93cf1ec51ee9

---
+++ libs/libXP/DataTable.cpp
@@ -0,0 +1,168 @@
+#include <QtGui>
+#include "DataTable.hpp"
+
+int DataTable::rowCount(const QModelIndex&) const {
+  return m_data.size();
+}
+
+int DataTable::columnCount(const QModelIndex&) const {
+  return m_colnames.size();
+}
+
+void DataTable::loadData(const QList<QVariant> & data) {
+  m_data = data;
+  emit layoutChanged();
+}
+
+QVariant DataTable::headerData(int section, Qt::Orientation orientation, int role) const {
+  if (role != Qt::DisplayRole) return QVariant();
+  if (orientation != Qt::Horizontal) return QVariant();
+  if (section < m_colnames.size())
+    return m_colnames[section];
+  return QVariant();
+}
+
+QVariant DataTable::data(const QModelIndex &index, int role) const {
+  if ((role == Qt::DecorationRole) && (index.column() == 1)) {
+    int row = index.row();
+    QList<QVariant> md(m_data[row].toList());
+    if (md.size() > 0) 
+      return md[0];
+    else
+      return QVariant();
+  }
+  if (role != Qt::DisplayRole)
+    return QVariant();
+  int row = index.row();
+  int col = index.column();
+  if ((row >= 0) && (row < m_data.size())) {
+    QList<QVariant> md(m_data[row].toList());
+    if ((col >= 0) && (col < md.size()))
+      return md[col];
+    return QVariant();
+  }
+  return QVariant();
+}
+
+void DataTable::sort(int column, Qt::SortOrder order) {
+  if (m_data.size() == 0) return;
+  // Determine the type of the data for this column
+  QList<QVariant> row(m_data[0].toList());
+  if (column >= row.size()) return;
+  switch(row[column].type()) {
+  case QVariant::Int:
+  case QVariant::Double:
+    {
+      QMap<double,QVariant> map;
+      for (int i=0;i<m_data.size();i++) 
+	map.insert(m_data[i].toList()[column].toDouble(),m_data[i]);
+      m_data = map.values();
+      break;
+    }
+  case QVariant::String:
+    {
+      QMap<QString,QVariant> map;
+      for (int i=0;i<m_data.size();i++)
+	map.insert(m_data[i].toList()[column].toString(),m_data[i]);
+      m_data = map.values();
+      break;
+    }
+  default:
+    return;
+  }
+  if (order == Qt::DescendingOrder) {
+    QList<QVariant> tmp;
+    for (int i=0;i<m_data.size();i++)
+      tmp.push_back(m_data[m_data.size()-1-i]);
+    m_data = tmp;
+  }
+  emit layoutChanged();
+}
+
+QVariant mkStats(bool flag, QString nm, int minv, int maxv) {
+  QList<QVariant> ret;
+  if (flag)
+    ret.push_back(QVariant(qApp->style()->standardPixmap(QStyle::SP_FileIcon)));
+  else
+    ret.push_back(QVariant(qApp->style()->standardPixmap(QStyle::SP_DirIcon)));
+  ret.push_back(QVariant(nm));
+  ret.push_back(QVariant(minv));
+  ret.push_back(QVariant(maxv));
+  return QVariant(ret);
+}
+
+void DataView::loadSettings(QSettings *set, QString tagname, QVector<int> default_cols) {
+  int n = model()->columnCount();
+  QList<QVariant> hidden(set->value(tagname+"/hidden").toList());
+  QList<QVariant> widths(set->value(tagname+"/widths").toList());
+  if ((hidden.size() >= n) || (widths.size() >= n)) {
+    for (int i=0;i<n;i++)
+      setColumnHidden(i,hidden[i].toBool());
+    for (int i=0;i<n;i++)
+      setColumnWidth(i,widths[i].toInt());
+  } else {
+    for (int i=0;i<n;i++)
+      setColumnHidden(i,true);
+    for (int i=0;i<default_cols.size();i++)
+      setColumnHidden(default_cols[i],false);
+  }
+  ((DataHeaderView*)header())->initializeFieldChooser();
+}
+
+void DataView::saveSettings(QSettings *set, QString tagname) {
+  int n = model()->columnCount();
+  QList<QVariant> hidden;
+  for (int i=0;i<n;i++)
+    hidden.push_back(QVariant(isColumnHidden(i)));
+  set->setValue(tagname + "/hidden",hidden);
+  QList<QVariant> widths;
+  for (int i=0;i<n;i++)
+    widths.push_back(QVariant(columnWidth(i)));
+  set->setValue(tagname + "/widths",widths);
+}
+
+
+void DataHeaderView::setColumnHidden(int col, bool stat) {
+  if(col <= 1) return;
+  m_fields[col-2]->setChecked(!stat);
+}
+
+void DataHeaderView::initializeFieldChooser() {
+  int col = model()->columnCount();
+  m_fields = new QAction*[col-2]; // two columns do not show up -- name & dirflag
+  m_popup = new QMenu;
+  for (int i=2;i<col;i++) {
+    m_fields[i-2] = new QAction(model()->headerData(i,Qt::Horizontal).toString(),this);
+    m_fields[i-2]->setCheckable(true);
+    m_fields[i-2]->setChecked(!isSectionHidden(i));
+    connect(m_fields[i-2],SIGNAL(toggled(bool)),this,SLOT(updateChecked(bool)));
+    m_popup->addAction(m_fields[i-2]);
+  }
+}
+
+void DataHeaderView::contextMenuEvent(QContextMenuEvent *e) {
+  m_popup->exec(e->globalPos());
+}
+
+void DataHeaderView::updateChecked(bool) {
+  for (int i=2;i<model()->columnCount();i++) {
+    bool vis = m_fields[i-2]->isChecked();
+    m_dview->setColumnHidden(i,!vis);
+    if (((QTreeView*)m_dview)->columnWidth(i) < 50)
+      m_dview->setColumnWidth(i,50);
+  }
+}
+
+void DataView::setModel(QAbstractItemModel *m) {
+  QTreeView::setModel(m);
+  m_header = new DataHeaderView(Qt::Horizontal,this);
+  m_header->setMovable(true);
+  m_header->setStretchLastSection(true);
+  m_header->setDefaultAlignment(Qt::AlignLeft|Qt::AlignVCenter);
+  m_header->setModel(m);
+  m_header->initializeFieldChooser();
+  setHeader(m_header);
+  setSortingEnabled(true);
+  setItemsExpandable(false);
+  setRootIsDecorated(false);
+}
---
+++ libs/libXP/DataTable.hpp
@@ -0,0 +1,60 @@
+#ifndef __DataTable_hpp__
+#define __DataTable_hpp__
+
+#include <QAbstractTableModel>
+#include <QTreeView>
+#include <QMenu>
+#include <QAction>
+#include <QHeaderView>
+#include <QSettings>
+
+class DataTable : public QAbstractTableModel {
+  Q_OBJECT
+public:
+  DataTable(const QStringList &column_names) : QAbstractTableModel(), m_colnames(column_names) {}
+  virtual ~DataTable() {}
+  void loadData(const QList<QVariant> &);
+  int rowCount(const QModelIndex&) const;
+  int columnCount(const QModelIndex&) const;
+  QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+  QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
+  void sort(int column, Qt::SortOrder order);
+private:
+  int m_columns;
+  QList<QVariant> m_data;
+  QStringList m_colnames;
+};
+
+class DataHeaderView;
+
+class DataView : public QTreeView {
+  Q_OBJECT
+public:
+  DataView(QWidget* parent) : QTreeView(parent) {}
+  virtual ~DataView() {}
+  void setModel(QAbstractItemModel *m);
+  void loadSettings(QSettings *set, QString tagname, QVector<int> default_cols);
+  void saveSettings(QSettings *set, QString tagname);
+private:
+  QMenu *m_popup;
+  QAction **m_fields;
+  DataHeaderView *m_header;
+}; 
+
+class DataHeaderView : public QHeaderView {
+  Q_OBJECT
+public:
+  DataHeaderView(Qt::Orientation o, DataView *parent) : QHeaderView(o,parent), m_dview(parent) {}
+  virtual ~DataHeaderView() {}
+  void initializeFieldChooser();
+  void contextMenuEvent(QContextMenuEvent *e);
+  void setColumnHidden(int col, bool stat);
+private:
+  QMenu *m_popup;
+  QAction **m_fields;
+  DataView *m_dview;
+private slots:
+  void updateChecked(bool);
+};
+
+#endif
