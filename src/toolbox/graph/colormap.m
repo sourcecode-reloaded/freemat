4c9ccff76c8965f244a71eb25e1c59ee108dcf04
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2009-07-14 06:06:29 +0000

    force refresh removed (not needed)
    git-svn-id: https://svn.code.sf.net/p/freemat/code/branches/FreeMat4@3671 58e251e6-400e-0410-b350-93cf1ec51ee9

--- src/toolbox/graph/colormap.m
+++ src/toolbox/graph/colormap.m
@@ -115,12 +115,3 @@ if (nargin == 0)
 else
   set(gcf,'colormap',A);
 end
-% Force a refresh of children
-force_refresh(get(gcf,'children'));
-
-function force_refresh(children)
-for i = 1:numel(children)
-  set(children(i),'tag',get(children(i),'tag'));
-  force_refresh(get(children(i),'children'));
-end
-
