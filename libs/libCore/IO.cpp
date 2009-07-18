c6bdb16ac8009529270a963ccfa5fecc87815fd2
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2009-07-18 04:27:21 +0000

    Fixed bug in test_load1.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/branches/FreeMat4@3690 58e251e6-400e-0410-b350-93cf1ec51ee9

--- libs/libCore/IO.cpp
+++ libs/libCore/IO.cpp
@@ -874,7 +874,7 @@ static ArrayVector LoadNativeFunction(int nargout, QString filename,
 //@{ test_load1.m
 //function test_val = test_load1
 //  v = [1,2,3,4;5,6,7,8;9,6,2,3];
-//  save('test_load1.txt','-ascii',v);
+//  save('test_load1.txt','-ascii','v');
 //  u = load('test_load1.txt','-ascii');
 //  test_val = issame(v,u);
 //@}
