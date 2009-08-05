a21b20f4ff85cd5f015b7cfa406273a789d4aaf8
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2009-08-05 18:20:18 +0000

    added test script.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/branches/FreeMat4@3742 58e251e6-400e-0410-b350-93cf1ec51ee9

---
+++ tools/testmod/tadd.m
@@ -0,0 +1,11 @@
+import('add.so','addArrays','addArrays','int32','int32 N, float[N] a, float[N] b, float[N] &c');
+import('add.so','sum_string','sum_string','int32','string a');
+import('add.so','upper_string','upper_string','int32','string &a');
+import('add.so','square_it','square_it','void','double &b');
+a = [3,2,3,1];
+b = [5,6,0,2];
+c = [0,0,0,0];
+d = addArrays(length(a),a,b,c);
+ts = 'Hello there, peoples!';
+e = sum_string(ts);
+f = upper_string(ts);
