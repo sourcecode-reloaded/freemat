b05c04e409b105615cc410626323916200eba061
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2009-08-21 04:02:47 +0000

    Removed REF prefix from lsame.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/branches/FreeMat4@3802 58e251e6-400e-0410-b350-93cf1ec51ee9

--- libs/libMath/libLAPACK_C/lsame.c
+++ libs/libMath/libLAPACK_C/lsame.c
@@ -12,7 +12,7 @@
 
 #include "f2c.h"
 
-logical REF_lsame_(char *ca, char *cb, ftnlen ca_len, ftnlen cb_len)
+logical lsame_(char *ca, char *cb, ftnlen ca_len, ftnlen cb_len)
 {
     /* System generated locals */
     logical ret_val;
