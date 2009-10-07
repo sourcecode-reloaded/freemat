4a6d93ce2e60a77ff0da3707776662b7b971b63a
geneing <geneing@58e251e6-400e-0410-b350-93cf1ec51ee9>
2009-10-07 05:20:05 +0000

    An improved license_check
    git-svn-id: https://svn.code.sf.net/p/freemat/code/branches/FreeMat4@3925 58e251e6-400e-0410-b350-93cf1ec51ee9

--- tools/license_check.py
+++ tools/license_check.py
@@ -1,33 +1,30 @@
 import os
 
-
+ignore_list = ['llvm', 'dependencies', 'levmar-2.3', 'libLAPACK_C',  'thirdparty', 'libBLAS_C', 'libARPACK_C']
 
 def visit( arg,  dirname,  names):
-   out = open('miss', 'a')
+    out = open('miss', 'a')
+    
+    for name in names:
+        (root, ext) = os.path.splitext(name)
 
-   for name in names:
-       (root, ext) = os.path.splitext(name)
-
-       if dirname.find('llvm') != -1:
-           continue
-       if dirname.find('thirdparty') != -1:
-           continue
-       if name.find('moc') != -1:
-           continue
-       if ext=='.cpp' or ext=='.hpp':
-           path = os.path.join(dirname, name)
-           licensed = False
-           f = open(path, 'r')
-           for line in f:
-               if line.find('Licensed under the GPL') !=-1 or line.find('GNU General Public License')!=-1:
-                   licensed=True
-           f.close()
-           if licensed==False:
-               print path
-               out.write(path)
-               out.write('\n')
-
-
-   out.close()
-
-os.path.walk('..',  visit,  '')
+        res=[(dirname.find(lst) != -1) for lst in ignore_list]
+        if any(res):
+            continue
+        if ext=='.m' or ext=='.cpp' or ext=='.c' or ext=='.h' or ext=='.hpp':
+            path = os.path.join(dirname, name)
+            licensed = False
+            f = open(path, 'r')
+            for line in f:
+                if line.find('GPL') !=-1 or line.find('GNU General Public License')!=-1:
+                    licensed=True
+            f.close()
+            if licensed==False:
+                print path
+                out.write(path)
+                out.write('\n')
+            
+            
+    out.close()
+    
+os.path.walk('c:\\freemat_src\\FreeMat4',  visit,  '')
