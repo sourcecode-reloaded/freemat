87f355dc361a95a397183b4502e87580d8d7910b
geneing <geneing@58e251e6-400e-0410-b350-93cf1ec51ee9>
2010-04-26 05:53:46 +0000

    In progress.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/branches/ArraySimplification@4006 58e251e6-400e-0410-b350-93cf1ec51ee9

--- libs/libMatC/JITAnalysis.cpp
+++ libs/libMatC/JITAnalysis.cpp
@@ -26,19 +26,41 @@
 #include "Context.hpp"
 #include "Interpreter.hpp"
 
-void JITAnalysis::add_variable( QString name, DataClass type, bool isScalar, bool isLoopVariable )
+void JITVariableTable::add_variable( QString name, DataClass type, bool isScalar, bool isLoopVariable )
 {
 	ArrayReference ptr(eval->getContext()->lookupVariable(name));
 	if (!ptr.valid()){
 		symbols.insertSymbol( name, VariableInfo( false, isScalar, 0, type ) );
-		dbout << "Defining vars" << '\n';
+		dbout << "Defining vars new: " << name << '\n';
 	}
 	else{
 		DataClass aclass = ptr->dataClass();
-		dbout << "Defining vars" << '\n';
+		dbout << "Defining vars existing " << name << '\n';
 	}
 
 }
+
+void JITVariableTable::rhs_index_assignment( QString name, const VariableInfo& lhs_type )
+{
+	ArrayReference ptr(eval->getContext()->lookupVariable(name));
+	if (!ptr.valid()){
+		add_variable( name, lhs_type.data_class(), lhs_type.isScalarVariable(), false );
+	}
+	else{
+		assert("TODO: check type",0);
+	}
+}
+
+void JITVariableTable::rhs_assignment( QString name, const VariableInfo& lhs_type )
+{
+	add_variable( name, lhs_type.data_class(), lhs_type.isScalarVariable(), false );
+}
+
+void JITVariableTable::new_loop_index( QString name, const VariableInfo& lhs_type )
+{
+	add_variable( name, lhs_type.data_class(), lhs_type.isScalarVariable(), true );
+}
+
 
 void JITAnalysis::ProcessTree( const Tree &t)
 {
@@ -84,8 +106,107 @@ void JITAnalysis::process_statement_type(const Tree &t)
 	}
 }
 
+void JITAnalysis::process_statement(const Tree & t) {
+	if (t.is(TOK_STATEMENT) && 
+		(t.first().is(TOK_EXPR) || t.first().is(TOK_SPECIAL) ||
+		t.first().is(TOK_MULTI) || t.first().is('=')))
+		throw Exception("JIT compiler doesn't work with verbose statements");
+	process_statement_type(t.first());
+}
+
+
+void JITAnalysis::process_block(const Tree & t) {
+	const TreeList &statements(t.children());
+	for (TreeList::const_iterator i=statements.begin();i!=statements.end();i++) 
+		process_statement(*i);
+}
+
+
 void JITAnalysis::process_assignment( const Tree& t )
 {
+	const Tree & s(t.first());
+	QString symbol_prefix("");
+	QString symname(symbol_prefix+s.first().text());
+
+	const Tree & rhs( t.second() );
+	VariableInfo rhs_type;
+
+	if( s.numChildren() == 1 ){
+		dbout << "Var: " << symname << " variable assignment on lhs\n";
+		rhs_type = process_expression( rhs );
+		variables.rhs_assignment( symname, rhs_type );
+	}
+	else{
+		dbout << "Var: " << symname << " indexed assignment on lhs\n";
+		dbout << "Num children " << s.numChildren() << "\n";
+		rhs_type = process_expression( rhs );
+		variables.rhs_index_assignment( symname, rhs_type );
+	}
+
+	//JITScalar rhs(compile_expression(t.second()));
+	//SymbolInfo *v = symbols.findSymbol(symname);
+	//if (!v) {
+	//	if (s.numChildren() == 1)
+	//		v = add_argument_scalar(symname,rhs,false);
+	//	else
+	//		v = add_argument_array(symname, true /*createIfMissing*/ );
+	//	if (!v) throw Exception("Undefined variable reference:" + symname);
+	//}
+	//if (s.numChildren() == 1) {
+	//	if (v->type != jit->TypeOf(rhs))
+	//		throw Exception("polymorphic assignment to scalar detected.");
+	//	if (!v->isScalar)
+	//		throw Exception("scalar assignment to array variable.");
+	//	jit->Store(rhs, v->address);
+	//	return;
+	//}
+	//if (s.numChildren() > 2)
+	//	throw Exception("multiple levels of dereference not handled yet...");
+	//if (v->isScalar)
+	//	throw Exception("array indexing of scalar values...");
+	//const Tree & q(s.second());
+	//if (!q.is(TOK_PARENS))
+	//	throw Exception("non parenthetical dereferences not handled yet...");
+	//if (q.numChildren() == 0)
+	//	throw Exception("Expecting at least 1 array reference for dereference...");
+	//if (q.numChildren() > 2)
+	//	throw Exception("Expecting at most 2 array references for dereference...");
+	//if (q.numChildren() == 1) {
+	//	JITScalar arg1 = jit->ToDouble(compile_expression(q.first()));
+	//	JITScalar success_code;
+	//	if (jit->IsDouble(v->type))
+	//		success_code = jit->Call(func_vector_store_double, this_ptr, jit->DoubleValue(v->argument_num), arg1, 
+	//		jit->ToType(rhs,v->type));
+	//	else if (jit->IsFloat(v->type))
+	//		success_code = jit->Call(func_vector_store_float, this_ptr, jit->DoubleValue(v->argument_num), arg1, 
+	//		jit->ToType(rhs,v->type));
+	//	else if (jit->IsBool(v->type))
+	//		success_code = jit->Call(func_vector_store_bool, this_ptr, jit->DoubleValue(v->argument_num), arg1, 
+	//		jit->ToType(rhs,v->type));
+	//	else
+	//		throw Exception("unhandled type for vector store");
+	//	handle_success_code(success_code);
+	//	return;
+	//} else if (q.numChildren() == 2) {
+	//	JITScalar arg1 = jit->ToDouble(compile_expression(q.first()));
+	//	JITScalar arg2 = jit->ToDouble(compile_expression(q.second()));
+	//	JITScalar success_code;
+	//	if (jit->IsDouble(v->type))
+	//		success_code = jit->Call(func_matrix_store_double, this_ptr, jit->DoubleValue(v->argument_num), 
+	//		arg1, arg2, jit->ToType(rhs,v->type));
+	//	else if (jit->IsFloat(v->type))
+	//		success_code = jit->Call(func_matrix_store_float, this_ptr, jit->DoubleValue(v->argument_num), 
+	//		arg1, arg2, jit->ToType(rhs,v->type));
+	//	else if (jit->IsBool(v->type))
+	//		success_code = jit->Call(func_matrix_store_bool, this_ptr, jit->DoubleValue(v->argument_num), 
+	//		arg1, arg2, jit->ToType(rhs,v->type));
+	//	else
+	//		throw Exception("unhandled type for matrix store");
+	//	handle_success_code(success_code);
+	//	return;    
+	//}
+
+
 	dbout << "Var: " << t.first().text() << " #children: " << t.numChildren() << '\n';
 }
 
@@ -98,19 +219,95 @@ void JITAnalysis::process_for_block( const Tree& t )
 
 	QString loop_variable_name( t.first().first().text() );
 	
-	add_variable( loop_variable_name, Double, true, true );
+	variables.new_loop_index( loop_variable_name, VariableInfo( false, true, 0, Double ) );
 
 	
 	dbout << "For Block child 2. Var: " << t.first().second().text() << " #children: " << t.first().second().numChildren() << '\n';
-	process_statement_type( t.second() );
+	process_block( t.second() );
 }
 
 void JITAnalysis::process_if_statement( const Tree& t )
 {
 }
 
-void JITAnalysis::process_expression( const Tree& t )
+VariableInfo JITAnalysis::process_variable( const Tree& t )
 {
+	dbout << "---->  process_variable: " << t.first().text() << " " << t.numChildren() << "\n";
+	VariableInfo vi( false, false, 0, Double );
+	return vi;
+}
+
+VariableInfo JITAnalysis::process_expression( const Tree& t )
+{
+	switch(t.token()) {
+		  case TOK_VARIABLE:     return process_variable(t);
+/*		  case TOK_REAL:
+		  case TOK_REALF:
+			  if( t.array().isScalar() ){
+				  switch( t.array().dataClass() ){
+					  case Bool:
+						  return jit->BoolValue( t.array().constRealScalar<bool>() );
+					  case Float:
+						  return jit->FloatValue( t.array().constRealScalar<float>() );
+					  case Double:
+						  return jit->DoubleValue( t.array().constRealScalar<double>() );
+					  default:
+						  throw Exception("Unsupported scalar type.");
+				  }
+			  }
+			  else
+				  throw Exception("Unsupported type.");
+		  case TOK_STRING:
+		  case TOK_END:
+		  case ':':
+		  case TOK_MATDEF: 
+		  case TOK_CELLDEF:      throw Exception("JIT compiler does not support complex, string, END, matrix or cell defs");
+		  case '+':
+			  return jit->Add(compile_expression(t.first()),compile_expression(t.second()));
+		  case '-': 
+			  return jit->Sub(compile_expression(t.first()),compile_expression(t.second()));
+		  case '*': 
+		  case TOK_DOTTIMES: 
+			  return jit->Mul(compile_expression(t.first()),compile_expression(t.second()));
+		  case '/': 
+		  case TOK_DOTRDIV:
+			  return jit->Div(compile_expression(t.first()),compile_expression(t.second()));
+		  case '\\': 
+		  case TOK_DOTLDIV: 
+			  return jit->Div(compile_expression(t.second()),compile_expression(t.first()));
+		  case TOK_SOR: 
+			  return compile_or_statement(t);
+		  case '|':
+			  return jit->Or(compile_expression(t.first()),compile_expression(t.second()));
+		  case TOK_SAND: 
+			  return compile_and_statement(t);
+		  case '&': 
+			  return jit->And(compile_expression(t.first()),compile_expression(t.second()));
+		  case '<': 
+			  return jit->LessThan(compile_expression(t.first()),compile_expression(t.second()));
+		  case TOK_LE: 
+			  return jit->LessEquals(compile_expression(t.first()),compile_expression(t.second()));
+		  case '>': 
+			  return jit->GreaterThan(compile_expression(t.first()),compile_expression(t.second()));
+		  case TOK_GE: 
+			  return jit->GreaterEquals(compile_expression(t.first()),compile_expression(t.second()));
+		  case TOK_EQ: 
+			  return jit->Equals(compile_expression(t.first()),compile_expression(t.second()));
+		  case TOK_NE: 
+			  return jit->NotEqual(compile_expression(t.first()),compile_expression(t.second()));
+		  case TOK_UNARY_MINUS: 
+			  return jit->Negate(compile_expression(t.first()));
+		  case TOK_UNARY_PLUS: 
+			  return compile_expression(t.first());
+		  case '~': 
+			  return jit->Not(compile_expression(t.first()));
+		  case '^':               throw Exception("^ is not currently handled by the JIT compiler");
+		  case TOK_DOTPOWER:      throw Exception(".^ is not currently handled by the JIT compiler");
+		  case '\'':              throw Exception("' is not currently handled by the JIT compiler");
+		  case TOK_DOTTRANSPOSE:  throw Exception(".' is not currently handled by the JIT compiler");
+		  case '@':               throw Exception("@ is not currently handled by the JIT compiler");
+		  default:                throw Exception("Unrecognized expression!");
+	*/}  
 }
 
 
--- libs/libMatC/JITAnalysis.hpp
+++ libs/libMatC/JITAnalysis.hpp
@@ -1,21 +1,21 @@
 /*
- * Copyright (c) 2009 Samit Basu
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- */
+* Copyright (c) 2009 Samit Basu
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+*/
 #ifndef __JITAnalysis_hpp__
 #define __JITAnalysis_hpp__
 #include "JIT.hpp"
@@ -25,39 +25,56 @@
 
 class VariableInfo {
 	bool isUndefined; 
-  bool isScalar;
-  int argument_num;
-  DataClass type;
+	bool isScalar;
+	int argument_num;
+	DataClass type;
 
-  VariableInfo(bool is_Undefined, bool is_scalar, int arg_num, DataClass base_type) :
-    isUndefined(is_Undefined), isScalar(is_scalar), argument_num(arg_num), type(base_type) {}
-  friend class JITAnalysis;
+	friend class JITVariableTable;
 
 public:
-  VariableInfo(const VariableInfo &other) { this->isUndefined = other.isUndefined; this->isScalar = other.isScalar; this->argument_num = other.argument_num; this->type = other.type; }
-  VariableInfo &operator=(const VariableInfo &other) { this->isUndefined = other.isUndefined; this->isScalar = other.isScalar; this->argument_num = other.argument_num; this->type = other.type; return *this;}
-  VariableInfo() {} //need default constructor for SymbolTable
+	VariableInfo(bool is_Undefined, bool is_scalar, int arg_num, DataClass base_type) :
+		isUndefined(is_Undefined), isScalar(is_scalar), argument_num(arg_num), type(base_type) {}
+	VariableInfo(const VariableInfo &other) { this->isUndefined = other.isUndefined; this->isScalar = other.isScalar; this->argument_num = other.argument_num; this->type = other.type; }
+	VariableInfo &operator=(const VariableInfo &other) { this->isUndefined = other.isUndefined; this->isScalar = other.isScalar; this->argument_num = other.argument_num; this->type = other.type; return *this;}
+	VariableInfo() {} //need default constructor for SymbolTable
+	bool isScalarVariable(void) const { return isScalar; }
+	void setScalar(bool Scalar) { isScalar = Scalar; }
+	DataClass data_class( void ) const { return type; }
+};
+
+class JITVariableTable{
+private:
+	Interpreter *eval;
+	FM::SymbolTable<VariableInfo> symbols;
+public:
+	JITVariableTable(Interpreter *p_eval) : eval(p_eval){};
+	void rhs_assignment( QString name, const VariableInfo& lhs_type );
+	void rhs_index_assignment( QString name, const VariableInfo& lhs_type );
+	void new_loop_index( QString name, const VariableInfo& lhs_type );
+	
+private:
+	void add_variable( QString name, DataClass type, bool isScalar, bool isLoopVariable = false );
 };
 
 class JITAnalysis{
 private:
 	JIT *jit;
 	Interpreter *eval;
-	FM::SymbolTable<VariableInfo> symbols;
+	JITVariableTable variables;
 
 public:
-	JITAnalysis(Interpreter *p_eval) : eval(p_eval) { jit = JIT::Instance(); }
+	JITAnalysis(Interpreter *p_eval) : variables( p_eval), eval(p_eval) { jit = JIT::Instance(); }
 	void ProcessTree( const Tree& t );
+
 private:
-
-	void add_variable( QString name, DataClass type, bool isScalar, bool isLoopVariable = false );
-
-
 	void process_statement_type( const Tree& t );
 	void process_assignment( const Tree& t );
 	void process_for_block( const Tree& t );
 	void process_if_statement( const Tree& t );
-	void process_expression( const Tree& t );
+	VariableInfo process_expression( const Tree& t );
+	void process_statement(const Tree & t);
+	void process_block(const Tree & t);
+	VariableInfo process_variable( const Tree& t );
 };
 
 #endif
